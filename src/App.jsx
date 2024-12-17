bb283ac9c397c71471129e5714fbd027cab07632
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-12-16 20:17:35 -0500

    draft

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Board from "./components/board";
+import './App.css';
+import React from 'react';
+import './components/style.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+     
+    </div>
+  );
+}
+
+export default App;
+
